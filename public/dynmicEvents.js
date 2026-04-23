0a1c40a805f6eb36828b8f2b55998811c048d50f
SEZ4 <151035853+SEZ4@users.noreply.github.com>
2026-04-23 19:05:00 +0300

    +

--- public/dynmicEvents.js
+++ public/dynmicEvents.js
@@ -2,14 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const addButton = document.getElementById('addform-button');
     const infoButton = document.getElementById('infoform-button');
+    const editButton = document.getElementById('editform-button');
 
     const addForm = document.querySelector('.addform-form');
     const infoForm = document.querySelector('.infoform-list');
+    const editForm = document.querySelector('.editform-form');
 
     addButton.addEventListener('click', function(event){
         event.stopPropagation();
         addForm.classList.add('listOpen');
         infoForm.classList.remove('listOpen');
+        editForm.classList.remove('listOpen');
     })
     addForm.addEventListener('click', function(event){
         event.stopPropagation();
@@ -18,12 +21,23 @@ document.addEventListener('DOMContentLoaded', function() {
         event.stopPropagation();
         infoForm.classList.add('listOpen');
         addForm.classList.remove('listOpen');
+        editForm.classList.remove('listOpen');
     })
     infoForm.addEventListener('click', function(event){
+        event.stopPropagation();
+    })
+    editButton.addEventListener('click', function(event){
+        event.stopPropagation();
+        editForm.classList.add('listOpen');
+        addForm.classList.remove('listOpen');
+        infoForm.classList.remove('listOpen');
+    })
+    editForm.addEventListener('click', function(event){
         event.stopPropagation();
     })
     document.addEventListener('click', function(){
         addForm.classList.remove('listOpen');
         infoForm.classList.remove('listOpen');
+        editForm.classList.remove('listOpen');
     })
 })
