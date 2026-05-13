5a4faa65bf99e00dad6ab05cb16232903a756537
SEZ4 <151035853+SEZ4@users.noreply.github.com>
2026-05-13 16:20:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,11 @@ app.post('/reset-week', async (req, res) => {
 })
 app.post('/streak-increment', async (req, res) => {
     try{
-        await pool.query(`UPDATE generalinfo SET suger_streak = COALESCE(suger_streak, 0) + 1 WHERE id = 1`);
+        await pool.query(`
+        UPDATE generalinfo
+        SET suger_streak = COALESCE(suger_streak, 0) + 1
+        WHERE id = 1;
+        `);
 
         res.send("Daily Streak increment");
     } catch(error){
